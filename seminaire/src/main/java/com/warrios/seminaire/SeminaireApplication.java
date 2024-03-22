64847af4b542ffb4653b0d8a696c4f6cc29b416f
massiresogore <105115249+massiresogore@users.noreply.github.com>
2024-03-23 00:36:44 +0100

    initialisation de spring

---
+++ seminaire/src/main/java/com/warrios/seminaire/SeminaireApplication.java
@@ -0,0 +1,13 @@
+package com.warrios.seminaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminaireApplication.class, args);
+	}
+
+}
