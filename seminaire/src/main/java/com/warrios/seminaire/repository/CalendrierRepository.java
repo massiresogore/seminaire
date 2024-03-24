b7637fa4c49a86c47af5ce7b8a8601e48ce645ce
cinhogaebili <cinhogaebili@gmail.com>
2024-03-24 21:39:00 +0100

    moditication repository

--- seminaire/src/main/java/com/warrios/seminaire/repository/CalendrierRepository.java
+++ seminaire/src/main/java/com/warrios/seminaire/repository/CalendrierRepository.java
@@ -2,5 +2,5 @@ package com.warrios.seminaire.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Calendrier extends JpaRepository<Calendrier, Integer> {
+public interface CalendrierRepository extends JpaRepository<CalendrierRepository, Integer> {
 }
