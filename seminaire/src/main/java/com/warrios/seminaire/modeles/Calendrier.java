da683945e3b0aef9ac181de28f7919809c9e8d91
Maycar Fezaud <mfezaud@gmail.com>
2024-03-25 11:22:58 -0700

    Ajout des differents relation sauf User-Even

--- seminaire/src/main/java/com/warrios/seminaire/modeles/Calendrier.java
+++ seminaire/src/main/java/com/warrios/seminaire/modeles/Calendrier.java
@@ -1,11 +1,16 @@
 package com.warrios.seminaire.modeles;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.*;
 
 @Entity
@@ -18,4 +23,10 @@ public class Calendrier {
 	private Integer id_calendrier;
 	private Date date_calendrier;
 
+	@OneToOne
+    @JoinColumn(name = "utilisateur_id")
+    private Utilisateur utilisateur;
+	
+	 @OneToMany(mappedBy = "calendrier")
+	 private List<Evenement> evenements = new ArrayList<>();
 }
