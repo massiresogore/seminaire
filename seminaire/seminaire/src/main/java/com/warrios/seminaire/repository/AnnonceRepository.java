83a2c1ea428b205703f7a6e66d333b2ccc44dd75
massiresogore <105115249+massiresogore@users.noreply.github.com>
2024-04-01 15:11:50 +0200

    Delete seminaire/seminaire directory

--- seminaire/seminaire/src/main/java/com/warrios/seminaire/repository/AnnonceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.warrios.seminaire.repository;
-
-
-import com.warrios.seminaire.modeles.Annonce;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnonceRepository extends JpaRepository<Annonce, Integer> {
-}
--- seminaire/seminaire/src/main/java/com/warrios/seminaire/repository/EvenementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.warrios.seminaire.repository;
-
-import com.warrios.seminaire.modeles.Evenement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EvenementRepository extends JpaRepository<Evenement, Integer> {
-}
