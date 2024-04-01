6dcdccf0d7c6d3e1e58cc5b543b7ce0e352075ac
massiresogore <105115249+massiresogore@users.noreply.github.com>
2024-04-01 15:18:10 +0200

    Delete seminaire directory

--- seminaire/src/main/java/com/warrios/seminaire/exception/ObjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.warrios.seminaire.exception;
-
-public class ObjectNotFoundException extends RuntimeException{
-
-    public ObjectNotFoundException(String objectName, Integer id) {
-        super("nous ne retrouvons pas "+ objectName+ " avec un id "+ id);
-    }
-}
